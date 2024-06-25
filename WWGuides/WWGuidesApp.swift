edfda41d8999cc2d4a0f371ddc3599bf2a001614
Denis Chakyr <ChakyrIT@gmail.com>
2024-06-25 04:31:52 +0300

    Initial Commit

---
+++ WWGuides/WWGuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWGuidesApp.swift
+//  WWGuides
+//
+//  Created by Денис on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WWGuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
